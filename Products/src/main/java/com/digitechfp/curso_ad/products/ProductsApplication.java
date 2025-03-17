ee71d823e9daeeb8c7df723ae5bfdae1f18b4f86
Martín Morente Vargas <martin.dev45@gmail.com>
2025-03-17 13:13:17 +0100

    Update

---
+++ Products/src/main/java/com/digitechfp/curso_ad/products/ProductsApplication.java
@@ -0,0 +1,13 @@
+package com.digitechfp.curso_ad.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApplication.class, args);
+	}
+
+}
